429e03c0af665177918853be95808f4da6aa349c
Jatin Singh <singh.jatin609@gmail.com>
2024-11-25 17:19:04 +0530

    update 3:

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000
 
 app.get("/",(req,res)=>{
-    res.send("Hello Komal,Aman,shrishti, This is a cloud deployed node application!!");
+    res.send("Hello Komal,Aman,shrishti and Shraddha, :)))))");
 })
 
 app.listen(PORT, ()=>{
