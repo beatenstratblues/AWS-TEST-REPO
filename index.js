3532f9b9f793cc3532f10e49f9f475ab9f407eaa
Jatin Singh <singh.jatin609@gmail.com>
2024-11-25 22:16:36 +0530

    upadte 7

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000
 
 app.get("/",(req,res)=>{
-    res.send("Hello this is my frist cloud application");
+    res.send("Hello this is my frist cloud application, Hello Shraddha, komal, papa, mummy and everybody....");
 })
 
 app.listen(PORT, ()=>{
