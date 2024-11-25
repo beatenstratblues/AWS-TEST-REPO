f925088d50b367ecebfc4a607889ae802dafea09
Jatin Singh <singh.jatin609@gmail.com>
2024-11-25 16:56:50 +0530

    upadate1

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 3000
 
 app.get("/",(req,res)=>{
     res.send("Hello this is my first express server, that is deployed on AWS, hello Pulla and friends, will join you");
 })
 
-app.listen(8000, ()=>{
+app.listen(PORT, ()=>{
     console.log("Server connected....")
 })
